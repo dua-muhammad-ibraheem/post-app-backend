f739af76faa34f49d601c74fcb4a14f07d540d90
DuaIbrahim <ibrahimdua0123@gmail.com>
2026-06-30 23:20:20 +0500

    Added CORS middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 
 require("dotenv").config();
-
+const cors = require("cors");
 const app = require("./app");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
-
+app.use(cors());
 // Connect Database
 connectDB();
 
