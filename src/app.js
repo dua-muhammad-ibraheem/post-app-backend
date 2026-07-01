311b4305a657f14de24a42ae142246444bc647dc
DuaIbrahim <ibrahimdua0123@gmail.com>
2026-07-01 21:42:55 +0500

    Complete JWT authentication with protected profile route and CORS setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 const authRoutes = require("./routes/authRoutes");
 
+// Middlewares
+app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
-
 require("dotenv").config();
-const cors = require("cors");
+
 const app = require("./app");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+
 // Connect Database
 connectDB();
 
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
-
